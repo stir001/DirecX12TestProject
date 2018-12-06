e5aecb906ca0194615cf076a7cf4e6e592fd3ab8
stir001 <baske.max@icloud.com>
2018-12-06 21:12:10 +0900

    テストコードを追加

--- WinMain.cpp
+++ WinMain.cpp
@@ -34,9 +34,9 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR, int cmdShow)
 
 		float cubeLength = 5.0f;
 
-		//auto plane = PhysicsSystem::Instance().CreateRigitBody(BulletShapeType::PLANE, { 0,1,0 });
-		//plane->SetCollisionState(BulletCollisionState::STATIC);
-		//plane->SetTag(0);
+		auto plane = PhysicsSystem::Instance().CreateRigitBody(BulletShapeType::PLANE, { 0,1,0 });
+		plane->SetCollisionState(BulletCollisionState::STATIC);
+		plane->SetTag(0);
 
 		std::random_device seed_generator;
 		std::mt19937 engin(seed_generator());
@@ -71,16 +71,14 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR, int cmdShow)
 				if (input.IsKeyDown(eVIRTUAL_KEY_INDEX_NUMPAD8))
 				{
 					s2Pos.z += 0.1f;
-					//test->Remove();
 				}
 
 				if (input.IsKeyDown(eVIRTUAL_KEY_INDEX_NUMPAD2))
 				{
 					s2Pos.z -= 0.1f;
-					test = nullptr;
 				}
 
-				test2->Translate(s2Pos);
+				test->Translate(s2Pos);
 
 				PhysicsSystem::Instance().DebugDraw();
 			}
