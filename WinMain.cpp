1103664760b08ffca5f0856cdd6315b48ae4634a
manabe <jba.baske.ichiro@gmail.com>
2019-02-13 17:44:40 +0900

    winmainを追加

---
+++ WinMain.cpp
@@ -0,0 +1,37 @@
+#include "GameScene.h"
+#include "Dx12MSLib.h"
+
+#include <Windows.h>
+#include <tchar.h>
+#include <memory>
+
+
+using namespace DirectX;
+
+const int WINDOW_WIDTH = 768;
+const int WINDOW_HEIGHT = 448;
+
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR, int cmdShow)
+{
+	auto& d12 = Dx12Ctrl::Instance();
+	d12.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	std::string wName = "1601295_真鍋奨一郎";
+	d12.SetWindowName(wName);
+	d12.Dx12Init(hInst);
+	
+	std::shared_ptr<DxInput> spInput(new DxInput());
+	GameScene* gameScene = new GameScene(spInput);
+
+	//メインループ
+	while (ProcessMessage()) {
+
+		gameScene->Run();
+
+	}
+
+	delete gameScene;
+
+	d12.Release();
+	Dx12Ctrl::Destroy();
+}
