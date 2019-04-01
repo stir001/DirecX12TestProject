4f2f0e8aa3c72f747c042ef9f5f3fced6eeecc4e
manabe <jba.baske.ichiro@gmail.com>
2019-04-01 10:50:37 +0900

    少し変更

--- WinMain.cpp
+++ WinMain.cpp
@@ -4,34 +4,28 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR, int cmdShow)
 {
 	Dx12CtrlInit(hInst);
 	{
+		auto model = LoadFMD("3DModel/FBX/hutyakiti_anim/Hutyakiti_hatON_animeVer.fmd");
+		model->SetScale(5.0f);
 		DxInput input;
-		auto& camera = GetCamera(0);
+		auto& camera = GetCamera();
+		camera->SetTarget(0.0f, 10.0f, 0.0f);
+		const float rotaRatio = 1.0f / 3.14159265f * 0.1f;
 		while (ProcessMessage()) {
 			input.UpdateKeyState();
 
-			if (input.IsKeyDown(VIRTUAL_KEY_INDEX::KEY_NUMPAD8))
+			if (input.IsKeyDown(VIRTUAL_KEY_INDEX::KEY_RIGHT))
 			{
-				
+				model->AddRotaY(-rotaRatio);
 			}
 			
-			if (input.IsKeyDown(VIRTUAL_KEY_INDEX::KEY_NUMPAD2))
+			if (input.IsKeyDown(VIRTUAL_KEY_INDEX::KEY_LEFT))
 			{
-				
+				model->AddRotaY(rotaRatio);
 			}
 
-			if (input.IsKeyDown(VIRTUAL_KEY_INDEX::KEY_NUMPAD4))
-			{
-				
-			}
-
-			if (input.IsKeyDown(VIRTUAL_KEY_INDEX::KEY_NUMPAD6))
-			{
-				
-			}
-			camera->DefaultMove(input);
+			model->Draw();
 		}
-		camera = nullptr;
 	}
-
 	Dx12CtrlEnd();
 }
+
